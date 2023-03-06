181f688ace93ec1703144932e4a0ef2c9e781366
maxim <maxim.lyoen@gmail.com>
2023-03-07 00:02:45 +0100

    ProjetWeb presque fini

--- Complément Web/mon-app/src/App.js
+++ Complément Web/mon-app/src/App.js
@@ -1,41 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
+  const [books, setBooks] = useState([]);
+  const [totalItems, setTotalItems] = useState(0);
+  const [startIndex, setStartIndex] = useState(0);
+  const [query, setQuery] = useState('');
+
+  const itemsPerPage = 10;
+  const currentPage = Math.floor(startIndex / itemsPerPage) + 1;
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  function handleSearch(event) {
+    setBooks([]);
+    setQuery(event.target.value);
+    setTotalItems(0);
+    setStartIndex(0);
+    if (event.target.value.trim() !== "") {
+      let requete =
+        `https://www.googleapis.com/books/v1/volumes?q=inauthor:${event.target.value}&startIndex=${startIndex}&maxResults=${itemsPerPage}`;
+      axios
+        .get(requete)
+        .then((response) => {
+          setBooks(response.data.items);
+          setTotalItems(response.data.totalItems);
+        })
+        .catch((error) => {
+          console.log("Erreur serveur" + error);
+        })
+    }
+  }
+
+  function handlePageChange(newStartIndex) {
+    setStartIndex(newStartIndex);
+    let requete =
+      `https://www.googleapis.com/books/v1/volumes?q=inauthor:${query}&startIndex=${newStartIndex}&maxResults=${itemsPerPage}`;
+    axios
+      .get(requete)
+      .then((response) => {
+        setBooks(response.data.items);
+      })
+      .catch((error) => {
+        console.log("Erreur serveur" + error);
+      })
+  }
+
   const element = (
     <div>
       <header>
         <h1>Titre du livre</h1>
-        <input type="text" id="title"/>
-        <button onClick={findBookByTitle}>Rechercher</button>
+        <input type="text" id="author" onChange={handleSearch}/>
       </header>
       <div id="result">
-        
+        {books.length > 0 ? books.map((book) => (
+          <div key={book.id}>
+            <h2>{book.volumeInfo.title}</h2>
+            <p>{book.volumeInfo.authors?.join(', ')}</p>
+            <a href={`https://books.google.fr/books?id=${book.id}`}>
+              <img src={book.volumeInfo.imageLinks?.thumbnail} alt={book.volumeInfo.title}/>
+            </a>
+          </div>
+        )) : (query.trim() !== "" && <p>Aucun ouvrage correspondant a votre recherche</p>)}
+        {totalItems > itemsPerPage && (
+          <footer className="pagination">
+            {startIndex > 0 && (
+              <button onClick={() => handlePageChange(startIndex - itemsPerPage)}>&lt;&lt; Page précédente</button>
+            )}
+            <p>{`${startIndex + 1}-${Math.min(startIndex + itemsPerPage, totalItems)}/${totalItems}`}</p>
+            {startIndex + itemsPerPage < totalItems && (
+              <button onClick={() => handlePageChange(startIndex + itemsPerPage)}>Page suivante &gt;&gt;</button>
+            )}
+          </footer>
+        )}
       </div>
     </div>
   );
-  function findBookByTitle(event){
-    const title = event.target.value;
-    const url = "https://www.googleapis.com/books/v1/volumes?q=inauthor:%22" + title + "%22";
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      /*affiche le titre et l'auteur*/
-      const result = document.getElementById("result");
-      result.innerHTML = "";
-      data.items.forEach(item => {
-        const title = item.volumeInfo.title;
-        const author = item.volumeInfo.authors[0];
-        const p = document.createElement("p");
-        p.innerHTML = title + " - " + author + " - <a href=\"https://books.google.fr/books?id=" + item.id + "\"><img src=\"" + item.volumeInfo.imageLinks.thumbnail + "\"></a>";
-        result.appendChild(p);
-      }
-      );
-    });
-  }
 
   return element;
-  
 }
 
 export default App;
